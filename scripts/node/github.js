a8b62c9c880aac772bdc5c5b518bceeab9bc2dd0
Brent Gorton <brent.gorton@cloudcoach.com>
2023-04-25 22:33:10 -0600

    Update github.js

--- scripts/node/github.js
+++ scripts/node/github.js
@@ -84,7 +84,7 @@ module.exports = {
 			owner: check.owner,
 			repo: check.repo,
 			name: check.name,
-			head_sha: check.sha,
+			head_sha: check.head_sha,
 			status: check.status,
 			output: check.output,
 			headers: {
@@ -96,49 +96,6 @@ module.exports = {
 
 	annotate: async function (check) {
 		const octokit = new Octokit();
-		await octokit.request('PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}', {
-			owner: 'OWNER',
-			repo: 'REPO',
-			check_run_id: 'CHECK_RUN_ID',
-			name: 'mighty_readme',
-			started_at: '2018-05-04T01:14:52Z',
-			status: 'completed',
-			conclusion: 'success',
-			completed_at: '2018-05-04T01:14:52Z',
-			output: {
-			title: 'Mighty Readme report',
-			summary: 'There are 0 failures, 2 warnings, and 1 notices.',
-			text: 'You may have some misspelled words on lines 2 and 4. You also may want to add a section in your README about how to install your app.',
-			annotations: [
-				{
-				path: 'README.md',
-				annotation_level: 'warning',
-				title: 'Spell Checker',
-				message: 'Check your spelling for \'banaas\'.',
-				raw_details: 'Do you mean \'bananas\' or \'banana\'?',
-				start_line: 2,
-				end_line: 2
-				},
-				{
-				path: 'README.md',
-				annotation_level: 'warning',
-				title: 'Spell Checker',
-				message: 'Check your spelling for \'aples\'',
-				raw_details: 'Do you mean \'apples\' or \'Naples\'',
-				start_line: 4,
-				end_line: 4
-				}
-			],
-			images: [
-				{
-				alt: 'Super bananas',
-				image_url: 'http://example.com/images/42'
-				}
-			]
-			},
-			headers: {
-			'X-GitHub-Api-Version': '2022-11-28'
-			}
-		})
+		await octokit.request('PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}', check);
 	}
 };
